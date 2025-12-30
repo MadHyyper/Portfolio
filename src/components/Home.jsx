a66aecc209b8176afa951070c0a83a2005bdcb0d
Conner Houdek <cmhoudek04@gmail.com>
2025-12-30 15:53:56 -0600

    Text update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="max-w-6xl mx-auto p-4">
             <p className="font-mono text-2xl text-white">
                 My name is <span className="text-yellow-200">Conner Houdek</span>. I'm a software engineer who enjoys 
-                <span className="text-yellow-200"> creating</span> and <span className="text-yellow-200">solving problems</span>. 
+                <span className="text-yellow-200"> creating</span> new projects and <span className="text-yellow-200">solving problems</span>. 
                 I've worked on a variety of projects including <span className="text-yellow-200">websites</span>, 
                 <span className="text-yellow-200"> android apps</span>, and <span className="text-yellow-200">embedded systems development</span>. 
                 I also have experience with <span className="text-yellow-200">managing databases</span>, <span className="text-yellow-200">CI/CD pipelines</span>,
