35786c6ffd8f5254b88acdd156a2474e91d3560b
Conner Houdek <cmhoudek04@gmail.com>
2026-01-18 16:24:05 -0600

    Added Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Background from "./components/Background.jsx";
 import Home from "./components/Home.jsx";
+import Skills from "./components/Skills.jsx";
 
 function App() {
 
   return (
-    <>
+    <div className="space-y-30">
       <Background />
       <Home />
-    </>
+      <Skills />
+    </div>
   )
 }
 
---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+import { FaJava } from "react-icons/fa";
+
+export default function skills() {
+    return (
+        <main style={{ position: 'relative', zIndex: 1 }}>
+            {/* Programming Languages */}
+            <h1 className="font-mono text-white pb-4">Skills</h1>
+        </main>
+    )
+}
