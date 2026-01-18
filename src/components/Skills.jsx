f30f022c57db8f4726cc43f66a6b58172f1a890b
Conner Houdek <cmhoudek04@gmail.com>
2026-01-18 16:45:45 -0600

    Created directory to store skills components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,24 @@
 import "../App.css";
-import { FaJava } from "react-icons/fa";
+import Languages from "./Skills/Languages";
+
+var state = 0;
 
 export default function skills() {
     return (
         <main style={{ position: 'relative', zIndex: 1 }}>
-            {/* Programming Languages */}
+            {/* Skills */}
             <h1 className="font-mono text-white pb-4">Skills</h1>
+            {function renderState() {
+                if (state == 0) {
+                    // Render languages
+                }
+                else if (state == 1) {
+                    // Render frameworks
+                }
+                else if (state == 2) {
+                    // Render other tools
+                }
+            }}
         </main>
     )
 }
---
+++ src/components/Skills/Languages.jsx
@@ -0,0 +1,10 @@
+import "../../App.css";
+import { FaJava } from "react-icons/fa";
+
+export default function Languages() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
